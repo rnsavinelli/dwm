64fbecbad2e0e943adf2c153ff74cf2f9d96f072
Roberto Nicolas Savinelli <rnsavinelli@openbsd.local>
2023-05-01 18:37:28 -0300

    openbsd build

--- config.def.h
+++ config.def.h
@@ -27,8 +27,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Chrome",   NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -95,6 +94,8 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    { 0, 0x1008ff13, spawn, SHCMD("sndioctl output.level=+0.005") },
+    { 0, 0x1008ff11 , spawn, SHCMD("sndioctl output.level=-0.005") },
 };
 
 /* button definitions */
