365e9f205299eb8dcfa888e253e264afbaef1927
Roberto Nicolás Savinelli <rns18@student.london.ac.uk>
2024-03-29 15:01:58 -0300

    update config

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 28;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "Cantarell:size=12" };
+static const char *fonts[]          = { "Cantarell:size=12", "fontawesome:size=12" };
 static const char dmenufont[]       = "Cantarell:size=12";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -29,7 +29,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class        instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",       NULL,       NULL,       1 << 1,       1,           -1 }
+	{ "Gimp",       NULL,       NULL,       0,            1,           -1 },
+	{ "Caja",       NULL,       NULL,       0,            1,           -1 },
+	{ "zoom",       NULL,       NULL,       1 << 6,       0,           -1 },
+	{ "discord",    NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "Slack",      NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -62,8 +66,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run_history", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-l", "10", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *calccmd[]  = { "st", "python", NULL };
-static const char *increasevolumecmd[]  = { "pamixer", "--increase", "1", NULL };
-static const char *decreasevolumecmd[]  = { "pamixer", "--decrease", "1", NULL };
+static const char *increasevolumecmd[]  = { "pamixer", "--increase", "2", NULL };
+static const char *decreasevolumecmd[]  = { "pamixer", "--decrease", "2", NULL };
 static const char *mutevolumecmd[] = { "pamixer", "-t", NULL };
 static const char *medplaypausecmd[] = { "playerctl", "play-pause", NULL };
 static const char *mednextcmd[] = { "playerctl", "next", NULL };
@@ -116,8 +120,8 @@ static const Key keys[] = {
     { 0,                            XF86XK_AudioMute,           spawn,          {.v = mutevolumecmd } },
     { 0,                            XF86XK_AudioPlay,           spawn,          {.v = medplaypausecmd } },
     { 0,                            XF86XK_AudioStop,           spawn,          {.v = medplaypausecmd } },
-    { 0,                            XF86XK_AudioPrev,           spawn,          {.v = mednextcmd } },
-    { 0,                            XF86XK_AudioNext,           spawn,          {.v = medprevcmd } },
+    { 0,                            XF86XK_AudioPrev,           spawn,          {.v = medprevcmd } },
+    { 0,                            XF86XK_AudioNext,           spawn,          {.v = mednextcmd } },
 };
 
 /* button definitions */
